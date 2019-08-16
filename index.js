afcd15a39e31df8b67145b0cfb835332a53c79f6
Candace Joy Metsker <candacejmetsker@gmail.com>
2019-08-16 14:22:07 -0400

    Updates 8-16

---
+++ index.js
@@ -0,0 +1,42 @@
+`use strict`;
+
+//-----link event listeners----
+
+function bioLink() {
+    $(`.navbar`).on("click", ".bio", function(event){
+        event.preventDefault();
+        window.alert("it's working!!");
+        bioAutoScroll();
+    });
+}
+
+function projectLink() {
+    (`$.project`).click(function(event) {
+        event.preventDefault();
+        projectsAutoScroll();   
+    });
+}
+
+//-----autoscroll functions------
+function bioAutoScroll() {
+    $('html, body').animate({
+        scrollTop: $(".projSection").offset().top
+    }, 1000);
+}
+
+function projectsAutoScroll() {
+
+}
+
+function contactAutoScroll() {
+
+}
+
+
+//-------startup functions------
+function runPortfolioPage() {
+    bioLink();
+    projectLink();
+}
+
+$(runPortfolioPage);
