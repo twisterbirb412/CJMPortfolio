4dbc9ef93d663e75db09c6d33609c7fb59dc9767
Candace Joy Metsker <candacejmetsker@gmail.com>
2019-08-18 15:11:35 -0400

    Final Resubmission

--- index.js
+++ index.js
@@ -5,13 +5,16 @@
 function bioLink() {
     $(`.navbar`).on("click", ".bio", function(event){
         event.preventDefault();
-        autoScroll('.bioSection') 
+        checkForDropdownShow()
+        autoScroll('.bioSection')
+
     });
 }
 
 function projectLink() {
     $(`.navbar`).on("click", ".projects", function(event) {
         event.preventDefault();
+        checkForDropdownShow()
         autoScroll('.projSection')   
     });
 }
@@ -19,10 +22,24 @@ function projectLink() {
 function contactLink() {
     $(`.navbar`).on("click", ".contact", function(event) {
         event.preventDefault();
-        window.alert('!!');
+        checkForDropdownShow()
         autoScroll('.footer') 
         
     });  
+}
+
+function hamburgerClick() {
+    $(`.navbar`).on("click", ".hamburger", function(event){
+        event.preventDefault();
+        toggleDropdownClass();
+    });
+}
+
+function dropdownLoseFocus() {
+    $('.dropdown').focusout(function(event) {
+        console.log("Focusout initiated");
+        checkForDropdownShow();
+    });
 }
 
 //-----autoscroll functions------
@@ -35,12 +52,28 @@ function autoScroll(className) {
     $('html, body').animate({scrollTop: xCoord}, 1000);
 }
 
+//-------mobile menu functions-------------
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function toggleDropdownClass() {
+    $('.dropdown-content').toggleClass('show');
+}
+
+function checkForDropdownShow() {
+    if ($('.dropdown-content').hasClass('show')==true) {
+        toggleDropdownClass();
+    }
+}
+
 
 //-------startup functions------
 function runPortfolioPage() {
     bioLink();
     projectLink();
     contactLink();
+    hamburgerClick();
+    dropdownLoseFocus();
 }
 
 $(runPortfolioPage);
